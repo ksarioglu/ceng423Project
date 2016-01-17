0b9c8becba823ce56855cc5b9194797f768a07ea
kubrasarioglu <kbrsarioglu@gmail.com>
2016-01-17 17:50:15 +0200

    web service side of the project

---
+++ Service1.svc.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Ceng423Project
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+       public string HashPassword(string input)
+        {
+            var sha1 = SHA1Managed.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] outputBytes = sha1.ComputeHash(inputBytes);
+
+
+            return BitConverter.ToString(outputBytes).Replace("-", "").ToLower();
+        }
+        public byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+       {  // Check arguments. 
+           if (plainText == null || plainText.Length <= 0)
+               throw new ArgumentNullException("plainText");
+           if (Key == null || Key.Length <= 0)
+               throw new ArgumentNullException("Key");
+           if (IV == null || IV.Length <= 0)
+               throw new ArgumentNullException("Key");
+           byte[] encrypted;
+           // Create an RijndaelManaged object 
+           // with the specified key and IV. 
+           using (RijndaelManaged rijAlg = new RijndaelManaged())
+           {
+               rijAlg.Key = Key;
+               rijAlg.IV = IV;
+
+               // Create a decrytor to perform the stream transform.
+               ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+               // Create the streams used for encryption. 
+               using (MemoryStream msEncrypt = new MemoryStream())
+               {
+                   using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                   {
+                       using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                       {
+
+                           //Write all data to the stream.
+                           swEncrypt.Write(plainText);
+                       }
+                       encrypted = msEncrypt.ToArray();
+                   }
+               }
+           }
+
+
+           // Return the encrypted bytes from the memory stream. 
+           return encrypted;
+       }
+    }
+}
